ee60d5ca1b2b748a64ef7d4623ce36f4af698d55
Mel Gorman <mgorman@suse.de>
2013-10-07 11:29:04 +0100

    sched/numa: Check current->mm before allocating NUMA faults
    task_numa_placement checks current->mm but after buffers for faults
    have already been uselessly allocated. Move the check earlier.
    
    [peterz@infradead.org: Identified the problem]
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Reviewed-by: Rik van Riel <riel@redhat.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1381141781-10992-27-git-send-email-mgorman@suse.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -903,8 +903,6 @@ static void task_numa_placement(struct task_struct *p)
 	int seq, nid, max_nid = -1;
 	unsigned long max_faults = 0;
 
-	if (!p->mm)	/* for example, ksmd faulting in a user's mm */
-		return;
 	seq = ACCESS_ONCE(p->mm->numa_scan_seq);
 	if (p->numa_scan_seq == seq)
 		return;
@@ -969,6 +967,10 @@ void task_numa_fault(int last_nid, int node, int pages, bool migrated)
 	int priv;
 
 	if (!sched_feat_numa(NUMA))
+		return;
+
+	/* for example, ksmd faulting in a user's mm */
+	if (!p->mm)
 		return;
 
 	/* For now, do not attempt to detect private/shared accesses */
